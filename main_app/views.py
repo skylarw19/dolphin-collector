b6cdf9724dc51f9b53d854a2cd634b71c633d679
Skylar Wong <skylar@Skylars-Air.lan>
2020-03-19 19:43:54 -0500

    add model

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Dolphin
 # Create your views here.
-
-class Dolphin:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dolphins = [
-  Dolphin('Lolo', 'tabby', 'foul little demon', 3),
-  Dolphin('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Dolphin('Raven', 'black tripod', '3 legged cat', 4)
-]
 
 def home(request):
     return HttpResponse('<h1>Hello World</h1>')
@@ -22,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def dolphins_index(request):
+  dolphins = Dolphin.objects.all()
   return render(request, 'dolphins/index.html', { 'dolphins': dolphins })
